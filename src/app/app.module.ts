e4ae919336159ecce470ca7d7189c138b327687a
Eric Rodrigues <er601432@gmail.com>
2022-10-25 18:36:39 -0300

    fix: using custom paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
+import { ManagerModule } from '@Modules/manager/manager.module';
+import { PlanModule } from '@Modules/plan/plan.module';
+import { ProductModule } from '@Modules/product/product.module';
+import { SubscriptionModule } from '@Modules/subscription/subscription.module';
+import { UserModule } from '@Modules/user/user.module';
+import { WaitlistModule } from '@Modules/waitlist/waitlist.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ManagerModule } from '../modules/manager/manager.module';
-import { PlanModule } from '../modules/plan/plan.module';
-import { ProductModule } from '../modules/product/product.module';
-import { SubscriptionModule } from '../modules/subscription/subscription.module';
-import { UserModule } from '../modules/user/user.module';
-import { WaitlistModule } from '../modules/waitlist/waitlist.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
