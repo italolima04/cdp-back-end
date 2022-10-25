68a328dfae385d604024e4513717eb7fc3492441
Ítalo Lima <italolima534@gmail.com>
2022-10-25 00:10:15 -0300

    fix: fixing conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+<<<<<<< HEAD
 import { UserModule } from './user/user.module';
 import { SubscriptionModule } from './subscription/subscription.module';
 import { ProductModule } from './product/product.module';
@@ -11,6 +12,11 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule, ManagerModule, WaitlistModule, PlanModule, ProductModule, SubscriptionModule],
+=======
+
+@Module({
+  imports: [],
+>>>>>>> f781211 (chore: initial structure of project)
   controllers: [AppController],
   providers: [AppService],
 })
